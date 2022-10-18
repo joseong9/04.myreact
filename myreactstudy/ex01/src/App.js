1f0e2d50d9046eb0526a2318750758f8d041ee80
joseong9 <jsg8825@gmail.com>
2022-10-18 09:37:02 +0900

    myreact

---
+++ myreactstudy/ex01/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Ex01 from './compo/ex01';
+
+function App() {  //APP이 부모 컴포넌트 -> EX01이 자식 컴포넌트
+  return (
+    <Ex01></Ex01>
+  );
+}
+
+export default App;
---
+++ myreactstudy/ex01/src/compo/ex01.js
@@ -0,0 +1,6 @@
+function Ex01(){
+    return(
+        <h1>첫번째 컴포</h1>
+    )
+}
+export default Ex01;
