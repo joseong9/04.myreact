b373ed55126830feaea809842794c3ca32440191
joseong9 <jsg8825@gmail.com>
2022-10-19 13:56:20 +0900

    myreact

--- myreactstudy/ex02/src/App.js
+++ myreactstudy/ex02/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Ex01 from './compo/Ex01';
+import Ex02 from './compo/Ex02';
+
 
 function App() {
   return (
-    <Ex01></Ex01>
+    //<Ex01></Ex01>
+    <Ex02></Ex02>
   );
 }
 
--- myreactstudy/ex02/src/compo/Ex01.js
+++ myreactstudy/ex02/src/compo/Ex01.js
@@ -1,14 +1,23 @@
+import {useState} from 'react'
+
 function Ex01(){
 
-    const array = ["눈", "바람", "비", "햇살"];
+    //const array = ["눈", "바람", "비", "햇살"];
+    const [array, setArray] = useState(["눈", "바람", "비", "햇살"])
+    function deleteData(e) {
+        //e.target.value;
+        setArray(array.filter(function(data, index){
+            return index != e.target.value;
+        }));
+    }
 
     return(
         <div>
             <ul>
                 {array.map(function(data, index){
                     
-                    return <li key={index}>{data}</li>
-                    
+                    return <li key={index}>{data}<button value={index} onClick={deleteData}>삭제</button></li>
+
                 })}
             </ul>
         </div>
